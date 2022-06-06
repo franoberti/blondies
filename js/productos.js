bfaa170edb9744961b3a40815ce1b8b2577e3645
Francisco Oberti <franoberti45@gmail.com>
2022-06-06 12:21:37 -0300

    Agrega JavaScript

---
+++ js/productos.js
@@ -0,0 +1,7 @@
+
+const chocolate1 = new Chocolate ("Oreo", "Chocolate blanco o negro relleno de oreos y dulce de leche", "Chico", 10, 250)
+const chocolate2 = new Chocolate ("Oreo", "Chocolate blanco o negro relleno de oreos y dulce de leche", "Mediano", 7, 400)
+const chocolate3 = new Chocolate ("Oreo", "Chocolate blanco o negro relleno de oreos y dulce de leche", "Grande", 13, 550)
+const chocolate4 = new Chocolate ("Doble", "Chocolate negro y chocolate blanco relleno con oreos o chocolinas", "Chico", 15, 250)
+const chocolate5 = new Chocolate ("Doble", "Chocolate negro y chocolate blanco relleno con oreos o chocolinas", "Mediano", 4, 400)
+const chocolate6 = new Chocolate ("Doble", "Chocolate negro y chocolate blanco relleno con oreos o chocolinas", "Grande", 22, 550)
